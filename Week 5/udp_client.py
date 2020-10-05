346b5147d8b47b54a57369c6741e81a97c31663e
Aditeya <aditeya.baral@gmail.com>
2020-10-05 11:02:42 +0530

    add ss

--- Week 5/udp_client.py
+++ Week 5/udp_client.py
@@ -3,10 +3,10 @@ from socket import *
 
 server_name = sys.argv[1].encode()
 server_port = int(sys.argv[2])
-client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket = socket(AF_INET, SOCK_DGRAM)
 
-sentence = input("Enter sentence: ")
+sentence = input("\nEnter sentence: ").encode()
 client_socket.sendto(sentence, (server_name, server_port))
-modified_sentence, server_address = client_socket.recv(2048)
-print(f"server [{server_address}]> {modified_sentence}")
+modified_sentence, server_address = client_socket.recvfrom(2048)
+print(f"{server_address[0]} > {modified_sentence.decode()}")
 client_socket.close()

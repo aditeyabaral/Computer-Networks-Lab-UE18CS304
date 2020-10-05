e87faccd7e04b1170ba8b8dd48003189626a495c
Aditeya Baral <aditeya.baral@gmail.com>
2020-10-05 11:04:32 +0530

    add  ss

--- Week 5/udp_server.py
+++ Week 5/udp_server.py
@@ -3,8 +3,8 @@ from socket import *
 
 server_name = "10.0.2.15"
 server_port = 12000
-server_socket = socket(AF_INET, SOCK_STREAM)
-server_socket.bind(("", server_port))
+server_socket = socket(AF_INET, SOCK_DGRAM)
+server_socket.bind((server_name, server_port))
 
 print(f"Server 10.0.2.15 is ready to receive on port {server_port}")
 
